4277ad0e770c4672f47de83b74ed2099827dcf74
Jonathan Osacky <osacky2@ng.acm.uiuc.edu>
2014-03-08 20:46:05 -0600

    added population to csv

--- getCities.py
+++ getCities.py
@@ -6,6 +6,7 @@ import re
 import csv
 
 newlines = re.compile(r"\s+", re.MULTILINE)
+commas = re.compile(",", re.MULTILINE)
 
 page = requests.get('http://www.reddit.com/r/chicago')
 tree = html.fromstring(page.text)
@@ -16,14 +17,19 @@ tree2 = html.fromstring(page2.text)
 numUsers = tree.xpath('//span[@class="number"]/text()')
 
 cities = tree2.xpath('//tr/td[@width="143"]/font/text()')
-print cities 
+population = tree2.xpath('//tr/td[@width="75"]/font/text()')
 
+print cities
+print population
+
+print cities 
 with open('cities.csv', 'wb') as csvfile:
         csvwriter = csv.writer(csvfile) 
         labels = (["City Name"]);
         csvwriter.writerow(labels)
-        for city in cities:
-                print newlines.sub("", city)
-                csvwriter.writerow([newlines.sub("", city)])
+        for n in range(len(cities)):
+			info = (newlines.sub("", cities[n]),newlines.sub("", population[n].replace(",","")))
+			print info
+			csvwriter.writerow(info)
 
 #print newlines.sub("", cities)
