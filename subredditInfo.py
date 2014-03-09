45b4005d93b4d5e5b0826377e8ed6a69e484dbf6
Nelson Osacky <nelson@osacky.com>
2014-03-08 23:14:21 -0600

    updates csv

--- subredditInfo.py
+++ subredditInfo.py
@@ -7,7 +7,11 @@ import csv
 with open('uscities.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-                cityName = row[1]
+                cityName = row[11]
 	        page = requests.get('http://www.reddit.com/r/' + cityName)
 	        tree = html.fromstring(page.text)
-	        print cityName + str(tree.xpath('//span[@class="number"]/text()'))
+                subscribers = tree.xpath('//span[@class="number"]/text()')
+                if len(subscribers) > 0 :
+	          print subscribers[0]
+                else:
+                  print '0'
