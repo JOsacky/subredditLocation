2cde2ceba6552c16a3ebca9dcc91e8a5fdf542e2
Nelson Osacky <nelson@osacky.com>
2014-03-08 21:48:39 -0600

    added wikipedia cities file with extra row

--- subredditInfo.py
+++ subredditInfo.py
@@ -4,10 +4,10 @@ from lxml import html
 import requests
 import csv
 
-with open('cities.csv', 'rb') as csvfile:
+with open('uscities.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-                cityName = row[0]
+                cityName = row[1]
 	        page = requests.get('http://www.reddit.com/r/' + cityName)
 	        tree = html.fromstring(page.text)
 	        print cityName + str(tree.xpath('//span[@class="number"]/text()'))
